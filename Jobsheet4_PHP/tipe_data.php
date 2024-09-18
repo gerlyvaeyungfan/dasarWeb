10c65bb579ef378e1c04fe6c88c58e92d0ce245d
gerly <gherly.ylrehg@gmail.com>
2024-09-18 14:13:02 +0700

    praktikum2.3

--- Jobsheet4_PHP/tipe_data.php
+++ Jobsheet4_PHP/tipe_data.php
@@ -17,3 +17,14 @@ $apakahSiswaSudahUjian = false;
 var_dump ($apakahSiswaLulus);
 echo "<br>";
 var_dump($apakahSiswaSudahUjian);
+
+echo "<br>";
+$namaDepan = "Ibnu";
+$namaBelakang = 'Jakaria';
+
+$namaLengkap = "{$namaDepan} {$namaBelakang}";
+$namaLengkap2 = $namaDepan . ' ' . $namaBelakang;
+
+echo "Nama Depan: {$namaDepan} <br>";
+echo 'Nama Belakang: ' . $namaBelakang . '<br>';
+echo $namaLengkap;
