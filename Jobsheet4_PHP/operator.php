6904c3306e12936bd2e0d9b281718ba347bd947e
gerly <gherly.ylrehg@gmail.com>
2024-09-21 06:06:32 +0700

    memperbaiki kode program operator menggunakan ternary

--- Jobsheet4_PHP/operator.php
+++ Jobsheet4_PHP/operator.php
@@ -7,7 +7,7 @@ echo "Hasil penjumlahan $a dan $b adalah : $hasilTambah<br>";
 $hasilKurang = $a - $b;
 echo "Hasil pengurangan $a dan $b adalah : $hasilKurang<br>";
 $hasilKali = $a * $b;
-echo "Hasil penrkaliann $a dan $b adalah : $hasilKali<br>";
+echo "Hasil perkalian $a dan $b adalah : $hasilKali<br>";
 $hasilBagi = $a / $b;
 echo "Hasil pembagian $a dan $b adalah : $hasilBagi<br>";
 $sisaBagi = $a % $b;
@@ -17,46 +17,45 @@ echo "Hasil perpangkatan dari $a pangkat $b adalah : $pangkat<br>";
 
 echo"<h3>Operator Perbandingan<br></h3>";
 $hasilSama = $a == $b;
-echo "Hasil dari $a sama dengan $b adalah : $hasilSama<br>";
+echo "Hasil dari $a sama dengan $b adalah : " . ($hasilSama ? 'true' : 'false') . "<br>";
 $hasilTidakSama = $a != $b;
-echo "Hasil dari $a tidak sama dengan $b adalah : $hasilTidakSama<br>";
+echo "Hasil dari $a tidak sama dengan $b adalah : " . ($hasilTidakSama ? 'true' : 'false') . "<br>";
 $hasilLebihKecil = $a < $b;
-echo "Hasil dari $a lebih kecil dari $b adalah : $hasilLebihKecil<br>";
+echo "Hasil dari $a lebih kecil dari $b adalah : " . ($hasilLebihKecil ? 'true' : 'false') . "<br>";
 $hasilLebihBesar = $a > $b;
-echo "Hasil dari $a lebih besar dari $b adalah : $hasilLebihBesar<br>";
+echo "Hasil dari $a lebih besar dari $b adalah : " . ($hasilLebihBesar ? 'true' : 'false') . "<br>";
 $hasilLebihKecilSama = $a <- $b;
-echo "Hasil dari $a lebih kecil sama dengan $b adalah : $hasilLebihKecilSama<br>";
+echo "Hasil dari $a lebih kecil sama dengan $b adalah : " . ($hasilLebihKecilSama ? 'true' : 'false') . "<br>";
 $hasilLebihBesarSama = $a >= $b;
-echo "Hasil dari $a lebih besar sama dengan $b adalah : $hasilLebihBesarSama<br>";
+echo "Hasil dari $a lebih besar sama dengan $b adalah : " . ($hasilLebihBesarSama ? 'true' : 'false') . "<br>";
 
 echo"<h3>Operator Logika<br></h3>";
 $hasilAnd = $a && $b;
-echo "Hasil penugasan AND dari $a dan $b adalah : $hasilAnd<br>";
+echo "Hasil penugasan AND dari $a dan $b adalah : " . ($hasilAnd ? 'true' : 'false' ) . "<br>";
 $hasilOr = $a || $b;
-echo "Hasil penugasan atau dari $a dan $b adalah : $hasilOr<br>";
+echo "Hasil penugasan atau dari $a dan $b adalah : " . ($hasilOr ? 'true' : 'false' ) . "<br>";
 $hasilNotA = !$a;
-echo "Hasil dari tidak sama dengan $a adalah : $hasilNotA<br>";
+echo "Hasil dari tidak sama dengan $a adalah : " . ($hasilNotA ? 'true' : 'false' ) . "<br>";
 $hasilNotB = !$b;
-echo "Hasil dari tidak sama dengan $b adalah : $hasilNotB<br>";
+echo "Hasil dari tidak sama dengan $b adalah : " . ($hasilNotB ? 'true' : 'false') . "<br>";
 
 echo"<h3>Operator Penugasan<br></h3>";
-$c = $a += $b;
-echo "Hasil dari penugasan 10 += $b adalah $c<br>";
-$d = $a -= $b;
-echo "Hasil dari penugasan $d -= $b adalah $d<br>";
-$e = $a *= $b;
-echo "Hasil dari penugasan $d *= $b adalah $e<br>";
-$f = $a /= $b;
-echo "Hasil dari penugasan $e /= $b adalah $f<br>";
-$g = $a %= $b;
-echo "Hasil dari penugasan $f %= $b adalah $g<br>";
+echo "a = 10<br>";
+echo "b = 5<br>";
+echo "Hasil dari penugasan a += b adalah a = ";$a += $b;echo "$a<br>";
+echo "Hasil dari penugasan a -= b adalah a = ";$a -= $b;echo "$a<br>";
+echo "Hasil dari penugasan a *= b adalah a = ";$a *= $b;echo "$a<br>";
+echo "Hasil dari penugasan a /= b adalah a = ";$a /= $b;echo "$a<br>";
+echo "Hasil dari penugasan a %= b adalah a = ";$a %= $b;echo "$a<br>";
 
 $a = 10;
 $b = 5;
 $hasilIdentik = $a === $b;
 $hasilTidakIdentik = $a !== $b;
-echo"<h3>Operator Identik<br></h3>";
-echo "Hasil identik dari $a dan $b adalah $hasilIdentik<br>";
-echo "Hasil tidak identik dari $a dan $b adalah $hasilTidakIdentik<br>";
+
+echo "<h3>Operator Identik<br></h3>";
+echo "Hasil identik dari $a dan $b adalah " . ($hasilIdentik ? 'true' : 'false') . "<br>";
+echo "Hasil tidak identik dari $a dan $b adalah " . ($hasilTidakIdentik ? 'true' : 'false') . "<br>";
+
 
 echo "<br><br><br><br>";
