609d108b67679993720cd5dc55a4d0867a7beb74
gerly <gherly.ylrehg@gmail.com>
2024-09-25 06:20:19 +0700

    Js5 - Praktikum4.3

--- Jobsheet5_PHP_Part_02/function.php
+++ Jobsheet5_PHP_Part_02/function.php
@@ -1,15 +1,15 @@
 <?php
 //membuat fungsi
-function perkenalan($nama, $salam) {
+function perkenalan($nama, $salam="Assalamualaikum"){
 echo $salam.", ";
-echo "Perkenalkan, nama saya " .$nama."<br/>";
+echo "Perkenalkan, nama saya ".$nama."<br/>";
 echo "Senang berkenalan dengan Anda<br/>";
 }
 //memanggil fungsi yang sudah dibuat
-perkenalan ("Hamdana","Hallo");
+perkenalan("Hamdana", "Hallo" );
 echo "<hr>";
 $saya = "Elok";
 $ucapanSalam = "Selamat pagi";
-//memanggil lagi
-perkenalan ($saya, $ucapanSalam) ;
+//memanggil lagi tanpa mengisi parameter salam
+perkenalan ($saya);
 ?>
