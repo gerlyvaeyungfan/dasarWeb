77f09c5cb814e9f3e5b086337adaa676b95b9dd6
gerly <gherly.ylrehg@gmail.com>
2024-09-25 05:29:29 +0700

    Js5 - Praktikum1

---
+++ Jobsheet5_PHP_Part_02/array_1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html> <head> </head> <body>
+<h2>Array Terindeks</h2>
+<?php
+$Listdosen=["Elok Nur Hamdana", "Unggul Pamenang", "Bagas Nugraha"];
+echo $Listdosen[2] . "<br>";
+echo $Listdosen[0] . "<br>";
+echo $Listdosen[1] . "<br>";
+echo "<h4>Menggunakan Loop</h4>";
+for ($i= 0;$i<count($Listdosen);$i++) {
+    echo $Listdosen[$i] . "<br>";
+}
+?>
+</body>
+</htmL>
