ed674c45176a80eab1c71fb79e4e687d78d2d9d1
gerly <gherly.ylrehg@gmail.com>
2024-09-25 09:16:57 +0700

    Js5 - Praktikum11.3 revisi

--- Jobsheet5_PHP_Part_02/global_post.php
+++ Jobsheet5_PHP_Part_02/global_post.php
@@ -8,7 +8,7 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 // collect value of input field
-  $name = $_POST['fname '];
+  $name = $_POST['fname'];
   if (empty($name)) {
     echo "Name is empty";
   } else {
