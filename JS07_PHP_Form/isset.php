6e06f3f6e0dbc63e5b09c41986daece113af92d1
gerly <gherly.ylrehg@gmail.com>
2024-10-09 05:54:26 +0700

    JS7 - Praktikum1.2

--- JS07_PHP_Form/isset.php
+++ JS07_PHP_Form/isset.php
@@ -5,4 +5,11 @@ if (isset($umur) && $umur >= 18) {
 } else {
     echo "Anda belum dewasa atau variabel 'umur' tidak ditemukan";
 }
+echo "<br>";
+$data = array("nama" => "Jane", "usisa" => 25);
+if (isset($data["nama"])) {
+    echo "Nama: " . $data["nama"];
+} else {
+    echo "Vawriabel 'nama' tidak ditemukan dalam araay";
+}
 ?>
