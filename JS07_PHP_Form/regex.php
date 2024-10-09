a98a7ea7ea0f8b306c712e800d8fcf0fa16a0770
gerly <gherly.ylrehg@gmail.com>
2024-10-09 12:56:17 +0700

    JS7 - Praktikum5.2

--- JS07_PHP_Form/regex.php
+++ JS07_PHP_Form/regex.php
@@ -1,9 +1,36 @@
-<?php
-$pattern = '/[a-z]/';
-$text = 'This is a Sample Text.';
-if (preg_match($pattern, $text)) {
-    echo "Huruf kecil ditemukan!";
-} else {
-    echo "Tidak ada huruf kecil!";
-}
-?>
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pattern Matching</title>
+</head>
+<body>
+    <h2>Hasil Pattern Matching</h2>
+
+    <?php
+    // Pencarian huruf kecil
+    $pattern = '/[a-z]/';
+    $text = 'This is a Sample Text.';
+    echo "<h3>Text: '$text'</h3>";
+    
+    if (preg_match($pattern, $text)) {
+        echo "<p>Huruf kecil ditemukan!</p>";
+    } else {
+        echo "<p>Tidak ada huruf kecil!</p>";
+    }
+
+    // Pencarian angka
+    $pattern = '/[0-9]+/';
+    $text = 'There are 123 apples.';
+    echo "<h3>Text: '$text'</h3>";
+    
+    if (preg_match($pattern, $text, $matches)) {
+        echo "<p>Cocokkan: " . $matches[0] . "</p>";
+    } else {
+        echo "<p>Tidak ada yang cocok</p>";
+    }
+    ?>
+</body>
+</html>
