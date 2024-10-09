cd6d4b7584207a2d054f4383f652880bc95c649b
gerly <gherly.ylrehg@gmail.com>
2024-10-09 15:24:18 +0700

    JS7 - Praktikum7.2

--- JS07_PHP_Form/form_validasi.php
+++ JS07_PHP_Form/form_validasi.php
@@ -5,14 +5,14 @@
     </head>
     <body>
         <h1>Form Input  dengan Validasi</h1>
-        <form methosd="POST" action="proses_validasi.php">
+        <form method="POST" action="proses_validasi.php">
             <label for="nama">Nama:</label>
             <input type="text" id="nama" name="nama">
-            <br>
+            <br><br>
 
             <label for="email">Email:</label>
             <input type="text" id="email" name="email">
-            <br>
+            <br><br>
             
             <input type="submit" value="Submit">
         </form>
