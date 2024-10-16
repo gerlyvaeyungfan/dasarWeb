d00db425e54d9a4550e7a0a79611ef21de487c4b
gerly <gherly.ylrehg@gmail.com>
2024-10-16 14:03:29 +0700

    JS8 - Praktikum1.9

--- JS08_PHP_Form_Cookies_Session/upload.php
+++ JS08_PHP_Form_Cookies_Session/upload.php
@@ -6,9 +6,13 @@ if(isset($_POST["submit"])){
 
     $allowedExtensions = array("jpg", "jpeg", "png", "gif");
     $maxsize = 5*1024*1024;
+
     if (in_array($fileType, $allowedExtensions) && $_FILES["myfile"]["size"] <= $maxsize) {
         if(move_uploaded_file($_FILES["myfile"]["tmp_name"], $targetfile)) {
             echo "File berhasil diunggah.";
+            
+            // Tampilkan gambar thumbnail setelah berhasil diunggah
+            echo "<br><img src='".$targetfile."' width='200' alt='Thumbnail'>";
         } else {
             echo "Gagal mengunggah file.";
         }
@@ -16,4 +20,3 @@ if(isset($_POST["submit"])){
         echo "File tidak valid atau melebihi ukuran maksimum yang diizinkan.";
     }
 }
-?>
