d326507571caee6df7fe51358132844f0c09a95f
gerly <gherly.ylrehg@gmail.com>
2024-10-16 15:33:51 +0700

    JS8 - Praktikum7.3

---
+++ JS08_PHP_Form_Cookies_Session/keranjangBelanja.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <body>
+            <h2> Keranjang Belanja </h2>
+
+            <?php
+                $beliNovel=$_COOKIE['beliNOvel'];
+                $beliBuku=$_COOKIE['beliBUku'];
+
+                echo "Jumlah Novel: " . $beliNovel ."<br>";
+                echo "Jumlah Buku". $beliBuku;
+            ?>
+        </body>
+    </head>
+</html>
