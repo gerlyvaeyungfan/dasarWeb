1bf9ad98d89b4b9d63324ae8423342ba5146638e
gerly <gherly.ylrehg@gmail.com>
2024-11-03 11:24:02 +0700

    JS10.2 - get_data.php FINAL

--- JS10_PHP_CRUD_and_Bootstrap/get_data.php
+++ JS10_PHP_CRUD_and_Bootstrap/get_data.php
@@ -1,11 +1,12 @@
 <?php
+
 session_start();
 include 'koneksi.php';
 include 'csrf.php';
 
 $id = $_POST['id'];
 $query = "SELECT * FROM anggota WHERE id=? ORDER BY id DESC";
-$sql = $dbl-> prepare($query);
+$sql = $db1-> prepare($query);
 $sql->bind_param('i', $id);
 $sql->execute();
 $res1 = $sql-> get_result();
@@ -17,5 +18,6 @@ while ($row = $res1-> fetch_assoc()) {
     $h['no_telp'] = $row["no_telp"];
 }
 echo json_encode($h);
-$dbl->close();
+$db1->close();
+
 ?>
