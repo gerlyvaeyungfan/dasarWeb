4d7259740d40b837217cd615ef2ed23d9b06d19c
gerly <gherly.ylrehg@gmail.com>
2024-11-03 11:26:54 +0700

    JS10.2 - proses.php FINAL

--- JS10_PHP_CRUD_and_Bootstrap/proses.php
+++ JS10_PHP_CRUD_and_Bootstrap/proses.php
@@ -2,39 +2,40 @@
 include('koneksi.php');
 
 $aksi = $_GET['aksi'];
-$nama = $_POST['nama'];
-$jenis_kelamin = $_POST['jenis_kelamin'];
-$alamat = $_POST['alamat'];
-$no_telp = $_POST['no_telp'];
+$nama = mysqli_real_escape_string($koneksi, $_POST['nama']);
+$jenis_kelamin = mysqli_real_escape_string($koneksi, $_POST['jenis_kelamin']);
+$alamat = mysqli_real_escape_string($koneksi, $_POST['alamat']);
+$no_telp = mysqli_real_escape_string($koneksi, $_POST['no_telp']);
 
-if($aksi == 'tambah') {
-    $query = "INSERT INTO anggota (nama, jenis_kelamin, alamat, no_telp) VALUES('$nama', '$jenis_kelamin','$alamat', '$no_telp')";
+if ($aksi == 'tambah') {
+    $query = "INSERT INTO anggota (nama, jenis_kelamin, alamat, no_telp) VALUES ('$nama', '$jenis_kelamin', '$alamat', '$no_telp')";
 
     if (mysqli_query($koneksi, $query)) {
-        header("Location: index.php");
+        header('Location: index.php');
         exit();
     } else {
-        echo "Gagal menambahkan data: " . mysqli_error($koneksi);    }
-} elseif($aksi == 'ubah') {
+        echo "Gagal menambahkan data: " . mysqli_error($koneksi);
+    }
+} elseif ($aksi == 'ubah') {
     if (isset($_POST['id'])) {
-        $id = $_POST['id'];
-
-        $query = "UPDATE anggota SET nama='$nama', jenis_kelamin='$jenis_kelamin', alamat='$alamat', no_telp='$no_telp' WHERE id = $id";
-
+        $id = mysqli_real_escape_string($koneksi, $_POST['id']);
+        
+        $query = "UPDATE anggota SET nama='$nama', jenis_kelamin='$jenis_kelamin', alamat='$alamat', no_telp='$no_telp' WHERE id='$id'";
+        
         if (mysqli_query($koneksi, $query)) {
-            header("Location: index.php");
+            header('Location: index.php');
             exit();
         } else {
             echo "Gagal mengupdate data: " . mysqli_error($koneksi);
         }
     } else {
-        echo "ID tidak valid";
+        echo "ID tidak valid.";
     }
-} elseif($aksi == 'hapus') {
+} elseif ($aksi == 'hapus') {
     if (isset($_GET['id'])) {
-        $id = $_GET['id'];
+        $id = mysqli_real_escape_string($koneksi, $_GET['id']);
 
-        $query = "DELETE FROM anggota WHERE id = $id";
+        $query = "DELETE FROM anggota WHERE id = '$id'";
 
         if (mysqli_query($koneksi, $query)) {
             header("Location: index.php");
