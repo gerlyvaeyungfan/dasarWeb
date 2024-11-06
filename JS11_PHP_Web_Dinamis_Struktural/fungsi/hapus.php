af7a9268ec102ecfa7288ed0495bf0fc2f477c73
gerly <gherly.ylrehg@gmail.com>
2024-11-06 14:32:01 +0700

    JS11 - P5.6

--- JS11_PHP_Web_Dinamis_Struktural/fungsi/hapus.php
+++ JS11_PHP_Web_Dinamis_Struktural/fungsi/hapus.php
@@ -16,6 +16,17 @@ if (!empty($_SESSION['username'])) {
         }
 
         header("Location: ./index.php?page=jabatan");
+    } else if (!empty($_GET['anggota'])) {
+        $id = antiinjection($koneksi, $_GET['id']);
+        $query = "DELETE FROM user WHERE user_id = '$id'";
+        
+        if (mysqli_query($koneksi, $query)) {
+            pesan('success', "Anggota Telah Terhapus.");
+        } else {
+            pesan('danger', "Data Login Terhapus Tetapi Data Anggota Todak Terhapus Karena: " . mysqli_error($koneksi));
+        }
+
+        header("Location: ./index.php?page=anggota");
     }
 }
 ?>
