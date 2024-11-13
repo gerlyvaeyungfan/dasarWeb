0ee9331630edd7e45ef73cb8f2e14ebd90b89556
gerly <gherly.ylrehg@gmail.com>
2024-11-13 12:33:39 +0700

    JS12 - P1.9

--- JS12_PHP_OOP/oop.php
+++ JS12_PHP_OOP/oop.php
@@ -72,50 +72,86 @@ $dog->sleep();
 $cat->meow();
 $dog->bark();
 
-interface Shape
-{
-    public function calculateArea();
+// interface Shape
+// {
+//     public function calculateArea();
+// }
+
+// class Circle implements Shape
+// {
+//     private $radius;
+
+//     public function __construct($radius)
+//     {
+//         $this->radius = $radius;
+//     }
+
+//     public function calculateArea()
+//     {
+//         return pi() * pow($this->radius, 2);
+//     }
+// }
+
+// class Rectangle implements Shape
+// {
+//     private $width;
+//     private $height;
+
+//     public function __construct($width, $height)
+//     {
+//         $this->width = $width;
+//         $this->height = $height;
+//     }
+
+//     public function calculateArea()
+//     {
+//         return $this->width * $this->height;
+//     }
+// }
+
+// function printArea(Shape $shape)
+// {
+//     echo "Area: " . $shape->calculateArea() . "<br>";
+// }
+
+// $circle = new Circle(5);
+// $rectangle = new Rectangle(4, 6);
+
+// printArea($circle);
+// printArea($rectangle);
+
+abstract class Shape {
+    abstract public function calculateArea();
 }
 
-class Circle implements Shape
-{
+class Circle extends Shape {
     private $radius;
 
-    public function __construct($radius)
-    {
+    public function __construct($radius) {
         $this->radius = $radius;
     }
 
-    public function calculateArea()
-    {
+    public function calculateArea(){
         return pi() * pow($this->radius, 2);
     }
 }
 
-class Rectangle implements Shape
-{
+class Rectangle extends Shape {
     private $width;
     private $height;
 
-    public function __construct($width, $height)
-    {
+    public function __construct($width, $height) {
         $this->width = $width;
         $this->height = $height;
     }
 
-    public function calculateArea()
-    {
+    public function calculateArea() {
         return $this->width * $this->height;
     }
-}
-
-function printArea(Shape $shape)
-{
-    echo "Area: " . $shape->calculateArea() . "<br>";
 }
 
 $circle = new Circle(5);
 $rectangle = new Rectangle(4, 6);
 
-printArea($circle);
-printArea($rectangle);
+echo "Area of Circle: " . $circle->calculateArea() . "<br>";
+echo "Area of Rectangle: " . $rectangle->calculateArea() . "<br>";
