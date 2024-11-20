6d6f931ec72f5eb54546dd766fdd047079068169
gerly <gherly.ylrehg@gmail.com>
2024-11-20 16:02:03 +0700

    JS13 - Pertanyaan 3

--- JS13_Materi_Tambahan/index.php
+++ JS13_Materi_Tambahan/index.php
@@ -1,13 +1,13 @@
 <?php
-    include('lib/Session.php');
-
-    $session = new Session();
-    if($session->get('is_login') !== true){
-        header('Location: login.php');
-    }
+include('lib/Session.php');
+$session = new Session();
+if ($session->get('is_login') !== true) {
+  header('Location: login.php');
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -20,62 +20,70 @@
   <!-- Theme style -->
   <link rel="stylesheet" href="adminlte/dist/css/adminlte.min.css">
 </head>
+
 <body class="hold-transition sidebar-mini">
-<!-- Site wrapper -->
-<div class="wrapper">
+  <!-- Site wrapper -->
+  <div class="wrapper">
     <!-- Navbar -->
-    <?php include('layout/header.php');?>
+    <?php include('layout/header.php'); ?>
     <!-- /.navbar -->
 
-  <!-- Main Sidebar Container -->
-  <aside class="main-sidebar sidebar-dark-primary elevation-4">
-    <!-- Brand Logo -->
-    <a href="adminlte/index3.html" class="brand-link">
-      <img src="adminlte/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">AdminLTE 3</span>
-    </a>
+    <!-- Main Sidebar Container -->
+    <aside class="main-sidebar sidebar-dark-primary elevation-4">
+      <!-- Brand Logo -->
+      <a href="adminlte/index3.html" class="brand-link">
+        <img src="adminlte/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <span class="brand-text font-weight-light">AdminLTE 3</span>
+      </a>
 
-    <!-- Sidebar -->
-    <?php include('layout/sidebar.php');?>
-    <!-- /.sidebar -->
-  </aside>
+      <!-- Sidebar -->
+      <?php include('layout/sidebar.php'); ?>/
+      <!-- /.sidebar -->
+    </aside>
 
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    
-    <?php
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+      <!-- Content Header (Page header) -->
+      <?php
       $page = isset($_GET['page']) ? $_GET['page'] : 'dashboard';
-      switch(strtolower($page)){
-        case 'dashboard': include('pages/dashboard.php'); break;
-        case 'buku'     : include('pages/buku.php'); break;
-        case 'kategori' : include('pages/kategori.php'); break;
-        case 'user'     : include('pages/user.php'); break;
-        default         : include('pages/404.php'); break;
+      switch (strtolower($page)) {
+        case 'dashboard':
+          include('pages/dashboard.php');
+          break;
+        case 'buku':
+          include('pages/buku.php');
+          break;
+        case 'kategori':
+          include('pages/kategori.php');
+          break;
+        case 'user':
+          include('pages/user.php');
+          break;
+        default:
+          include('pages/404.php');
+          break;
       }
-    ?>
+      ?>
+      <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
+    <!-- footer -->
+    <?php include('layout/footer.php'); ?>
 
-    <!-- /.content -->
+    <!-- Control Sidebar -->
+    <aside class="control-sidebar control-sidebar-dark">
+      <!-- Control sidebar content goes here -->
+    </aside>
+    <!-- /.control-sidebar -->
   </div>
-  <!-- /.content-wrapper -->
+  <!-- ./wrapper -->
 
-  <?php include('layout/footer.php');?>
-
-  <!-- Control Sidebar -->
-  <aside class="control-sidebar control-sidebar-dark">
-    <!-- Control sidebar content goes here -->
-  </aside>
-  <!-- /.control-sidebar -->
-</div>
-<!-- ./wrapper -->
-
-<!-- jQuery -->
-<script src="adminlte/plugins/jquery/jquery.min.js"></script>
-<!-- Bootstrap 4 -->
-<script src="adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- AdminLTE App -->
-<script src="adminlte/dist/js/adminlte.min.js"></script>
-<!-- AdminLTE for demo purposes -->
-<script src="adminlte/dist/js/demo.js"></script>
+  <!-- jQuery -->
+  <script src="adminlte/plugins/jquery/jquery.min.js"></script>
+  <!-- Bootstrap 4 -->
+  <script src="adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <!-- AdminLTE App -->
+  <script src="adminlte/dist/js/adminlte.min.js"></script>
+  <!-- AdminLTE for demo purposes -->
+  <script src="adminlte/dist/js/demo.js"></script>
 </body>
-</html>
